26a93624d952bdc1ca1268a07378d11afafbbeda
GabrielPy28 <gabrielparenas27@gmail.com>
2023-10-14 17:26:25 -0400

    adding changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # File-Encryption
+
+## First steps
+* Clone the repository
+* It is required to have Microsoft Visual C++ 14.0 or higher installed,  Get it with "Microsoft C++ Build Tools": https://visualstudio.microsoft.com/visual-cpp-build-tools/ 
+* Install dependencies: "pip install -r requirements.txt"
+
+## Run the Code:
+* Create a new IOTA node and generate a seed:
+    ```
+        iota_node = Iota('https://nodes.iota.org')
+        seed = iota_node.get_new_address()
+    ```
+* Replace the values of `YOUR_SERVER_ADDRESS` and `YOUR_SEED` in the code with the server address of your IOTA node and the  seed you generated.
+* Write and run the following code:
+    ```
+        python main.py
+    ```
+* enter the file path, example: `path/to/your/file_name` 
+* enter the path + the name of the file where it is saved, example: `path/to/save/decrypted/file_name`
+* The decrypted file will be saved as `path/to/save/decrypted/file`.
---
+++ main.py
@@ -0,0 +1,57 @@
+from iota import Iota, ProposedTransaction, Tag, TryteString, Address
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64
+
+#---------------------------------------------  Cliente ----------------------------------------------------------#
+# Initialize IOTA API
+api = Iota('https://nodes.iota.org:443', seed='YOUR_SEED', security_level=2)
+
+# File Source and Destination Paths
+file = input('Enter the path of the file: ')
+path_save = input('Enter the destination path to save the file: ')
+
+# Encrypt the file
+def encrypt_file(file_path):
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_EAX)
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return base64.b64encode(ciphertext), base64.b64encode(key), base64.b64encode(cipher.nonce)
+
+# Send the encrypted file to the server
+def send_file(encrypted_file, key, nonce):
+    message = f'{encrypted_file.decode()},{key.decode()},{nonce.decode()}'
+    transaction = ProposedTransaction(address='YOUR_SERVER_ADDRESS', message=TryteString.from_unicode(message), tag=Tag(b'ENCRYPTED_FILE'))
+    api.send_transfer([transaction])
+
+# Encrypt and send the file
+encrypted_file, key, nonce = encrypt_file(file)
+send_file(encrypted_file, key, nonce)
+
+#--------------------------------------------- Servidor ----------------------------------------------------------#
+# Decrypt the file
+def decrypt_file(encrypted_file, key, nonce):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_data = cipher.decrypt(encrypted_file)
+    return decrypted_data
+
+# Fetch and decrypt the file from the tangle
+def fetch_and_decrypt_file():
+    transactions = api.find_transactions(addresses=['YOUR_SERVER_ADDRESS'], tags=['ENCRYPTED_FILE'])
+    transaction = api.get_transaction_objects(transactions['hashes'])
+    message = transaction[0]['signatureMessageFragment'].decode()
+    encrypted_file, key, nonce = message.split(',')
+    encrypted_file = base64.b64decode(encrypted_file)
+    key = base64.b64decode(key)
+    nonce = base64.b64decode(nonce)
+    decrypted_file = decrypt_file(encrypted_file, key, nonce)
+    return decrypted_file
+
+# Fetch and decrypt the file
+decrypted_file = fetch_and_decrypt_file()
+
+# Save the decrypted file
+with open(path_save, 'wb') as file:
+    file.write(decrypted_file)
